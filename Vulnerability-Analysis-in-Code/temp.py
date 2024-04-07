f74e88450c8623d0e4fced5b0dc79f767de32f81
Shantanu Modhave <shantanu.modhave2021@vitbhopal.ac.in>
2024-04-07 22:06:24 +0530

    MLSA project

---
+++ Vulnerability-Analysis-in-Code/temp.py
@@ -0,0 +1,11 @@
+import os
+
+def delete_file(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print("File deleted successfully.")
+    else:
+        print("The file does not exist.")
+
+file_path = input("Enter the path of the file to delete: ")
+delete_file(file_path)
